e6f6422475b29c70a996e9661ea22ceee8e76f81
dit02 <dit@imac-7.local>
2019-03-28 14:21:06 +0900

    Initial Commit

---
+++ Alien lmageView 02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alien lmageView 02
+//
+//  Created by dit02 on 2019. 3. 28..
+//  Copyright © 2019년 dit02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
